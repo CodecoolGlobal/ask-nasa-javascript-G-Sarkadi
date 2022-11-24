58bb15f82e4c3c332cf56194151f5946eb9c8c7a
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-11-24 11:10:40 +0100

    Create basic card with dummy data

--- nasa-fetch-api/src/Card.js
+++ nasa-fetch-api/src/Card.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import "./cards.css"
 
 const Card = () => {
     // const [title, setTitle] = useState()
@@ -12,30 +13,42 @@ const Card = () => {
     
     const URL = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&date=2022-11-01`;
 
-    useEffect(() => {
-      const controller = new AbortController();
-      const signal = controller.signal;
+  //   useEffect(() => {
+  //     const controller = new AbortController();
+  //     const signal = controller.signal;
   
-          fetch(URL, {
-              signal: signal
-          })
-          .then((response) => response.json())
-          .then(data => setData(data));
+  //         fetch(URL, {
+  //             signal: signal
+  //         })
+  //         .then((response) => response.json())
+  //         .then(data => setData(data));
 
-      return () => {
-          // cancel the request before component unmounts
-          controller.abort();
-      };
-  }, []);
+  //     return () => {
+  //         // cancel the request before component unmounts
+  //         controller.abort();
+  //     };
+  // }, [URL]);
+
+
+  const dummyData = {
+    "date": "2022-02-02",
+    "explanation": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit possimus pariatur excepturi facilis necessitatibus, aut cumque perspiciatis corporis aliquam officia ipsum repellat neque maiores libero dicta sapiente sunt totam similique.",
+    "url": "https://apod.nasa.gov/apod/image/2211/LDN1251v7social1024.png",
+    "title": "Fake title"
+  }
+
 
   console.log(data)
 
 
   return (
     <div>
-      <p>
-        hello
-      </p>
+      <h2>
+        {dummyData.title}
+      </h2>
+      <p>{dummyData.date}</p>
+      <img src={dummyData.url} alt="nice stars" className="starPicture"/>
+      <p className="starExplanation">{dummyData.explanation}</p>
     </div>
   )
 }
