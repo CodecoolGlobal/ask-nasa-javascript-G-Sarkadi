64fd1e0302e061376134217fc959b30bd0fd2e93
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-11-24 13:31:14 +0100

    Delete dead code and comments

--- nasa-fetch-api/src/Card.js
+++ nasa-fetch-api/src/Card.js
@@ -1,44 +1,35 @@
 import { useEffect, useState } from "react"
 import Picture from "./Picture";
 
-const Card = ({date}) => {
-  // const [title, setTitle] = useState()
-  // const [date, setDate] = useState()
-  // const [url, setUrl] = useState()
-  // const [explanation, setExplanation] = useState()
+const Card = ({ date }) => {
   const [data, setData] = useState({});
-
-  
 
   const URL = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&date=${date}`;
 
-    useEffect(() => {
-      const controller = new AbortController();
-      const signal = controller.signal;
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
 
-          fetch(URL, {
-              signal: signal
-          })
-          .then((res) => res.json())
-          .then(res => setData(res));
+    fetch(URL, {
+      signal: signal
+    })
+      .then((res) => res.json())
+      .then(res => setData(res));
 
-      return () => {
-          // cancel the request before component unmounts
-          controller.abort();
-      };
+    return () => {
+      // cancel the request before component unmounts
+      controller.abort();
+    };
   }, [URL]);
 
 
   const dummyData = {
     "date": date,
     "explanation": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit possimus pariatur excepturi facilis necessitatibus, aut cumque perspiciatis corporis aliquam officia ipsum repellat neque maiores libero dicta sapiente sunt totam similique.",
-    "url": "https://www.youtube.com/embed/7dh5VL5YGoA",
+    "url": "https://apod.nasa.gov/apod/image/2211/LDN1251v7social1024.png",
     "title": "Fake title",
-    "media_type": "video"
+    "media_type": "image"
   }
-
-
-  console.log(data)
 
   function formatDate(data) {
     let dateObject = new Date(data.date + "z");
@@ -55,7 +46,7 @@ const Card = ({date}) => {
       <p>
         {formatDate(data)}
       </p>
-      <Picture url={data.url} media_type={data.media_type}/>
+      <Picture url={data.url} media_type={data.media_type} />
       <h4>
         {data.title}
       </h4>
