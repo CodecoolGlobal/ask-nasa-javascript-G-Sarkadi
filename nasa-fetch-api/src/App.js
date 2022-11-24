2d4e2d70ac6632ea7c41d2e5fe01b585454a393a
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-11-24 15:20:38 +0100

    Fix date selection bug, uses the correct date

--- nasa-fetch-api/src/App.js
+++ nasa-fetch-api/src/App.js
@@ -1,26 +1,19 @@
 import './App.css';
-import Title from './Title';
 import Card from './Card';
 import DateInput from './DateInput';
 import { useState } from 'react';
 
 function App() {
-  // const [currDate, setCurrDate] = useState();
-  const [selectedDate, setSelectedDate] = useState();
+  const [selectedDate, setSelectedDate] = useState("");
   const newDate = new Date();
   let currentDate = `${newDate.getFullYear()}-${newDate.getMonth() + 1}-${newDate.getDate()}`;
-
-  let date = currentDate;
-
-  console.log("currdate " + currentDate)
-  console.log("selected date " + selectedDate)
   
   return (
     <>
       <div className="container">
-        <Title />
+        <h1>Astronomy Picture of the Day (by NASA)</h1>
         <DateInput date={selectedDate} currentDate={currentDate} setSelectedDate={setSelectedDate}/>
-        <Card date={selectedDate}/>
+        <Card date={selectedDate ?? currentDate}/>
       </div>
       <footer>
         2022, Sarkadi Gergely
--- nasa-fetch-api/src/Title.js
+++
@@ -1,9 +0,0 @@
-
-const Title = () => {
-
-  return (
-    <h1>Astronomy Picture of the Day (by NASA)</h1>
-  )
-}
-
-export default Title
