e2f8bbb5ef0d3e72b46fd28ef25bf5c841673c7f
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-11-25 14:05:41 +0100

    Add proper modal window

--- nasa-fetch-api/src/components/gallery/Modal.js
+++ nasa-fetch-api/src/components/gallery/Modal.js
@@ -2,38 +2,40 @@ import React from "react";
 import "./Modal.css";
 
 function Modal({ setOpenModal, modalContent }) {
-  return (
-    <div className="modalBackground">
-      <div className="modalContainer">
-        <div className="titleCloseBtn">
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-          >
-            X
-          </button>
+    return (
+        <div className="modalBackground">
+            <div className="modalContainer">
+                <div className="titleCloseBtn">
+                    <button
+                        onClick={() => {
+                            setOpenModal(false);
+                        }}
+                    >
+                        X
+                    </button>
+                </div>
+                <div className="title">
+                    <h2>{modalContent.title}</h2>
+                </div>
+                <div className="body">
+                    <div className="picture">
+                        <img src={modalContent.url} alt={modalContent.title} width="700px" />
+                    </div>
+                    <p>{modalContent.explanation}</p>
+                </div>
+                <div className="footer">
+                    <button
+                        onClick={() => {
+                            setOpenModal(false);
+                        }}
+                        id="cancelBtn"
+                    >
+                        Close
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className="title">
-          <h1>{modalContent.title}</h1>
-        </div>
-        <div className="body">
-          <p>{modalContent.explanation}</p>
-        </div>
-        <div className="footer">
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-            id="cancelBtn"
-          >
-            Cancel
-          </button>
-          <button>Continue</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Modal;
